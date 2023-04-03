6dc97721f49c70f28ce0683d02becbffb304094c
fiteice <85375361+fiteice@users.noreply.github.com>
2023-04-03 20:55:17 +0700

    Create Akucintarimuru.py

---
+++ Akucintarimuru.py
@@ -0,0 +1,19 @@
+print("1.penjumlahan+")
+print("2.pengurangan-")
+print("3.perkalian*")
+print("4.pembagian/")
+Papan= input("input pilihan sistem ")
+Elaina= eval(input("angka pertama: "))
+megawati= eval(input("angka kedua: "))
+if Papan == '1':
+  hasil= Elaina+megawati
+  print(hasil)
+if Papan == '2':
+  hasil= Elaina-megawati
+  print(hasil)
+if Papan == '3':
+  hasil= Elaina*megawati
+  print(hasil)
+if Papan == '4':
+  hasil= Elaina/megawati
+  print(hasil)
